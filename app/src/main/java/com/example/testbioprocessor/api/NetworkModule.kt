67666bcd8462f036b8ac22c0574ff655bf5667ae
Алтунин Артем <artem.altunin@gazprombank.ru>
2025-10-08 12:44:10 +0300

    add timeout

--- app/src/main/java/com/example/testbioprocessor/api/NetworkModule.kt
+++ app/src/main/java/com/example/testbioprocessor/api/NetworkModule.kt
@@ -97,9 +97,9 @@ object NetworkModule {
             Log.d(TAG, "mTLS client created successfully")
 
             return OkHttpClient.Builder()
-                .connectTimeout(60, TimeUnit.SECONDS)
-                .readTimeout(60, TimeUnit.SECONDS)
-                .writeTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
                 .sslSocketFactory(sslContext.socketFactory, trustManager)
                 .hostnameVerifier { _, _ -> true }
                 .build()
